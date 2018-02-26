514817dbad31a6da6af04fc4f69fd654454c46e8
Unknown <wod.moshkin@gmail.com>
2018-02-26 16:18:39 +0200

    Adding localstorage functionality

--- WebApp/src/scripts/movieajax.js
+++ WebApp/src/scripts/movieajax.js
@@ -8,10 +8,10 @@ $('#movies').click(function() {
     $('#infoto').hide();
     let test = JSON.parse(localStorage.savedMovies);
 
-    console.log(test);
+    // console.log(test);
     let all = '';
     for (let i = 0; i < test.length; i++) {
-        all += test[ i ].Title + '; ' + test[ i ].Year + '; ' + test[ i ].Awards + '<br>';
+        all += test[i].Title + '; ' + test[i].Year + '; ' + test[i].Awards + '<br>';
     }
     $('#collection').html(all);
     $('#collection').show();
@@ -43,37 +43,63 @@ $('#add-movie').click(function() {
         return;
     }
 
+
     let persistentData = window.localStorage.getItem('savedMovies');
     let parsedPersistentData = JSON.parse(persistentData);
 
     if (parsedPersistentData == null) {
         let array = [];
         array.push(parsedData);
+
         window.localStorage.setItem('savedMovies', JSON.stringify(array));
+        $('#myMessage').html(`Film ${parsedData.Title} were added! `);
+
+
     } else {
-        parsedPersistentData.push(parsedData);
-        window.localStorage.setItem('savedMovies', JSON.stringify(parsedPersistentData));
+        var sMovies = JSON.parse(localStorage.savedMovies);
+        var filer = sMovies.filter(function(x) {
+            return x.Title == parsedData.Title;
+        })
+        if (filer.length < 1) {
+            parsedPersistentData.push(parsedData);
+            window.localStorage.setItem('savedMovies', JSON.stringify(parsedPersistentData));
+            $('#myMessage').html('Film were added in to collection');
+            
+        }
+        else{
+            $('#myMessage').html('Sorry you have alredy this film');
+        }
     }
 
     $('#add-movie').hide();
 
     window.sessionStorage.clear();
+
+
+
 });
 
 function callApi(title) {
     $.get(`http://www.omdbapi.com/?t=${title}&apikey=1c4993a8`).then(renderHtml);
-
+    //----------Test
+    var dData = $.get(`http://www.omdbapi.com/?apikey=1c4993a8&s=${title}&type=movie`);
+    
+    console.log(dData);
+    
+///-------End of test
     function renderHtml(data) {
+        // console.log(data);
         let title = data.Title;
         let year = data.Year;
         let director = data.Director;
 
         $('#my-img-tag-0')
             .attr('src', data.Poster)
-            .css({
-                width: '200px',
-                height: '200px'
-            });
+        // .css({
+        //     width: '300px',
+        //     height: '450px'
+        // });
+
 
         // if (JSON.parse(localStorage.getItem('currentMovie')) !== undefined) {
         //     console.log('hide');
@@ -100,14 +126,39 @@ function callApi(title) {
         //     $('#add-movie').show();
         // }
 
-        $('#add-movie').show();
+
 
         if (title !== undefined || year !== undefines) {
             $('.result').html('Title: "' + title + '", ' + 'Year: ' + year +
                 ', ' + 'Director: ' + director + '  ' + '<br>');
         };
-
+        $('#add-movie').show();
         $('#infoto').show();
+
+        // check(title);
 
     };
 };
+
+function check(title) {
+    if (localStorage.savedMovies === null) {
+        console.log('Sorry!');
+        return false;
+    } else {
+        let sMovies = JSON.parse(localStorage.savedMovies);
+        for (let i = 0; i < sMovies.length; i++) {
+            let titleBase = sMovies[i].Title;
+            if (title == titleBase) {
+                $('#add-movie').hide();
+                console.log('hidden');
+                $('#myMessage').html('Sorry! Youve already have this movie!');
+                return true;
+            } else {
+                return false;
+            }
+            // console.log
+        }
+        console.log('check');
+    }
+
+}
