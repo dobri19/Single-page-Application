ab115f5e1ee2bc26cf52f843d30528185c3bf464
Dobri V <dobri19@users.noreply.github.com>
2018-03-01 11:22:25 +0200

    Update search.js

--- WebApp/src/scripts/search.js
+++ WebApp/src/scripts/search.js
@@ -3,6 +3,7 @@
         if (event.keyCode === 13) {
             const value = $(this).val().trim();
             $('#dynamic').empty();
+            event.preventDefault();
             api.callApi(value);
         }
     });
